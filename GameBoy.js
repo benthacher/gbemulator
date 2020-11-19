fa2c97de85d0ffc12e0719bb1bec29f18992c2fb
Ben Thacher <8000ice@gmail.com>
2020-11-18 23:28:00 -0500

    reset() method in GameBoy class initializes regs

--- GameBoy.js
+++ GameBoy.js
@@ -7,7 +7,12 @@ class GameBoy {
 
     reset() {
         // RAM.fill(0); // specifically do NOT clear RAM, because the actual game boy doesn't do this
+        // resets RAM and registers according to 
+        for (const [addr, val] of Object.entries(INITIAL_RAM))
+            RAM.write(addr, val);
         
+        this.CPU.registers8 = INITIAL_REG8.slice(0);
+        this.CPU.registers16 = INITIAL_REG16.slice(0);
     }
 
     powerOn() {
